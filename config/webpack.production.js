4aacc3aedda97cc0004bf939070cf408fc090f46
dshster <dmitry@shvalyov.ru>
2019-10-06 00:01:25 +0300

    [SCB-3] Разработка webpack конфигурации для продакшена.

---
+++ config/webpack.production.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const root = (file = '') => path.join(__dirname, '../', file);
+
+const output = root('public');
+
+module.exports = {
+  entry: {
+    application: [
+      root('src/application/index.js'),
+    ],
+  },
+  output: {
+    path: output,
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.jsx?$/,
+      exclude: [/node_modules(?!\/webpack-dev-server)/, /public/],
+      resolve: { extensions: ['.js', '.jsx'] },
+      use: [{
+        loader: 'babel-loader',
+        options: {
+          presets: [['@babel/preset-env', { modules: false }], '@babel/react'],
+        },
+      }],
+    }],
+  },
+  plugins: [
+    new CleanWebpackPlugin({
+      verbose: true,
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Scratch Book',
+      template: root('src/application/view/index.html'),
+    }),
+  ],
+};
